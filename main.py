981e08b34a26ac83bf65d05fdcf5998e9ad9c9c9
Kamal <ksanjeewa@hfmarkets.com>
2025-11-15 15:35:01 +0530

    add annotation

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, HTTPException, Path, Query
 from schemas import BandBase, GenreURLChoices, BandWithId, BandCreate
-from fastapi import HTTPException
 
 app = FastAPI(
     title="FastAPI",
     description="Sample FastAPI application",
     version="0.1.0",
 )
+
 
 @app.get("/")
 async def read_root() -> dict[str, str]:
@@ -23,13 +25,20 @@ BANDS = [
     {"id": 2, "name": "AC/DC", "genre": "Electronic"},
     {"id": 3, "name": "The Beatles", "genre": "Hip-Hop"},
     {"id": 4, "name": "Queen", "genre": "Rock"},
-    {"id": 5, "name": "Nirvana", "genre": "Rock", "albums": [{
-        "title": "Nevermind",
-        "release_date": "1991-09-24",
-        "artist": "Nirvana",
-        "price": 10.99,
-        "is_available": True
-    }]},
+    {
+        "id": 5,
+        "name": "Nirvana",
+        "genre": "Rock",
+        "albums": [
+            {
+                "title": "Nevermind",
+                "release_date": "1991-09-24",
+                "artist": "Nirvana",
+                "price": 10.99,
+                "is_available": True,
+            }
+        ],
+    },
     {"id": 6, "name": "Red Hot Chili Peppers", "genre": "Metal"},
     {"id": 7, "name": "The Rolling Stones", "genre": "Electronic"},
     {"id": 8, "name": "The Who", "genre": "Metal"},
@@ -38,18 +47,28 @@ BANDS = [
 
 
 @app.get("/bands")
-async def bands(genre: GenreURLChoices | None = None, has_albums: bool = False ) -> list[BandWithId]:
+async def bands(
+    genre: GenreURLChoices | None = None,
+    has_albums: bool = False,
+    q: Annotated[str | None, Query(max_length=10)] = None,
+) -> list[BandWithId]:
     band_list = [BandWithId(**b) for b in BANDS]
     if genre:
         band_list = [b for b in band_list if b.genre.value.lower() == genre.value]
     if has_albums:
         band_list = [b for b in band_list if len(b.albums) > 0]
+    if q:
+        band_list = [
+            b for b in band_list if q.lower() in b.name.lower()
+        ]
 
     return band_list
 
 
 @app.get("/bands/{band_id}")
-async def band(band_id: int) -> BandWithId:
+async def band(
+    band_id: Annotated[int, Path(title="The band ID", openapi_examples="1")],
+) -> BandWithId:
     band_list = [BandWithId(**b) for b in BANDS]
     band = next((b for b in band_list if b.id == band_id), None)
     if band is None:
@@ -60,8 +79,6 @@ async def band(band_id: int) -> BandWithId:
 @app.post("/bands")
 async def create_band(band_data: BandCreate) -> BandWithId:
     id = BANDS[-1]["id"] + 1
-    band = BandWithId(id = id, **band_data.model_dump()).model_dump()
+    band = BandWithId(id=id, **band_data.model_dump()).model_dump()
     BANDS.append(band)
     return band
-
-
